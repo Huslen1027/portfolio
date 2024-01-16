86738012ab73d99996d9efb692fee0b5a16fdfb7
Huslen Batzorig <23LP2437@G304-CC0074.local>
2024-01-16 16:47:06 +0800

    mai

---
+++ src/pages/portfolio.js
@@ -0,0 +1,16 @@
+import { Header } from "@/components/Header";
+
+const Portfolio = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Experience />
+      <Work />
+      <Contant />
+      <Footer />
+    </>
+  );
+};
